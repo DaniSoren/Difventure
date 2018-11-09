9adb94475b8b606d3f6bf76044d9c193f8662ce0
Rasmus Steen Hanghøj <raxypony@gmail.com>
2018-11-09 13:04:51 +0100

    upgrade v2.0

---
+++ Difventure/Assets/OpenDoor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{ 
+    int rot = 0;
+	// Use this for initialization
+	void Start ()
+    {
+        GameObject player = GameObject.Find("player");
+        movement playerscript = player.GetComponent<movement>();
+        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+       
+        Loop();
+    }
+    void Loop()
+    {
+        if (GetComponent<movement>().pickuptrue)
+        {
+            if (rot < 60)
+            {
+                transform.Rotate(new Vector3(0f, 90f, 0f) * Time.deltaTime);
+                rot++;
+            }
+        }
+    }
+}
--- Difventure/Assets/movement.cs
+++ Difventure/Assets/movement.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class movement : MonoBehaviour
 {
 
-	// Use this for initialization
-	void Start ()
+    Animator Anim;
+    public float speed = 6f;
+    public bool pickuptrue = false;
+    // Use this for initialization
+    void Start ()
     {
-		
-	}
+        Anim = GetComponent<Animator>();
+    }
 	
 	// Update is called once per frame
 	void Update ()
@@ -18,10 +22,25 @@ public class movement : MonoBehaviour
         float moveVertical = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(-moveHorizontal*Time.deltaTime, 0.0f, -moveVertical*Time.deltaTime);
-        Vector3 rotate = new Vector3(moveHorizontal*Time.deltaTime,0.0f, 0.0f);
+      
 
-        transform.Translate(movement*2);
-        transform.Rotate(rotate);
+        transform.Translate(movement*speed);
+        
 
+        Animating(moveHorizontal, moveVertical);
+    }
+    void Animating(float moveHorizontal, float moveVertical)
+    {
+        bool walking = moveHorizontal != 0f || moveVertical != 0f;
+
+        Anim.SetBool ("IsWalking", walking);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            pickuptrue = true;
+        }
     }
 }
