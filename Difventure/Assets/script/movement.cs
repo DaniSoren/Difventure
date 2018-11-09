873c371f9372c557f07cea27abba8467591e6cb0
Rasmus Steen Hanghøj <raxypony@gmail.com>
2018-11-09 14:22:04 +0100

    døren fucking virker nu drenge!!!

---
+++ Difventure/Assets/script/movement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class movement : MonoBehaviour
+{
+
+  
+    public float speed = 6f;
+    public float pickups;
+    // Use this for initialization
+    void Start ()
+    {
+       
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(-moveHorizontal*Time.deltaTime, 0.0f, -moveVertical*Time.deltaTime);
+      
+
+        transform.Translate(movement*speed);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            pickups++;
+        }
+    }
+}
